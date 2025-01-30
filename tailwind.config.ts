3ecb0e92ca30c739fe612e1bbf1a37da96d3c8bd
William Oliveira Silva <williamsilva20062005@gmail.com>
2025-01-30 16:13:15 -0300

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,11 +7,11 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      container: {
+        center: true,
+        padding: "2rem",
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
