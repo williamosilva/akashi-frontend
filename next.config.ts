18b12809491bf588fee5e0b2294927aa66e5282d
William Oliveira Silva <williamsilva20062005@gmail.com>
2025-03-03 10:32:50 -0300

    refactor: simplify Next.js configuration and enhance metadata structure for improved SEO

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  i18n: {
-    locales: ["default", "en", "pt"],
-    defaultLocale: "default",
-    localeDetection: false,
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
